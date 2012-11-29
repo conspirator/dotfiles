a930a99e316084df6fd525914cb021073ac6bc96
Christopher Webb <cwebb@groupon.com>
2012-11-29 09:11:50 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Overview
+These are my dotfiles, and this is what I use to personalize my computer. There are many others like it, but this one is mine.
+
+# Install
+**Warning**: Please understand that this file will remove *some* of your dotfiles to install the ones included in this repo.
+
+**Only proceed if you know what you're doing.**
+
+```
+$  cd ~/
+$  git clone https://github.com/conspirator/dotfiles.git
+$  dotfiles/install.sh
+```
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ## ~/dotfiles/install.sh
+#
+# Removes *some* previous symlinks and creates new ones for files included with this repo.
+#
+# Author: Christopher Webb <hello@conspirator.co>
+# Website: http://conspirator.co
+# Updated: 11/29/12
+#
+
+
+# WARNING: THIS WILL REMOVE *SOME* PREVIOUSLY ESTABLISHED SYMLINKS. PROCEED WITH CAUTION
+echo "Removing old symlinks..."
+rm -f ~/.gitconfig
+rm -f ~/.gitignore
+rm -f ~/.zshrc
+
+
+echo "Creating git related symlinks..."
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitignore ~/.gitignore
+
+
+echo "Creating zsh related symlinks..."
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+
+
+echo "Finished."
